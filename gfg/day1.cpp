5b41a0e002b7e0cb7d47163cbfc5ce3731161039
ganeshbhandarkar <gbgb1bgbg@gmail.com>
2021-04-23 00:12:18 +0530

    update

---
+++ gfg/day1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void prank(long long a[], int n){
+        // code here
+        for(int i=0;i<n;i++){
+            a[i] += (a[a[i]]%n)*n;
+        }
+        
+        for(int i=0;i<n;i++){
+            a[i]/=n;
+        }
+    }
+};
